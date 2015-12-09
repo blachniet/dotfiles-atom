4f53e334ddc67ce381f087c631132c753e0a04d0
Brian Lachniet <blachniet@gmail.com>
2015-12-09 09:28:02 -0500

    Fix symlink script for Windows

--- symlink.ps1
+++ symlink.ps1
@@ -1,1 +1,5 @@
-iex "cmd /c mklink /H `"$HOME/.atom/keymap.cson`" `"$(Join-Path $PSScriptRoot keymap.cson)`""
+$link = (Join-Path $HOME ".atom\keymap.cson")
+if (Test-Path $link){
+  mv $link "$link.$(Get-Date -f yyMMddhhmmss).old"
+}
+iex "cmd /c mklink /H `"$link`" `"$(Join-Path $PSScriptRoot keymap.cson)`""
